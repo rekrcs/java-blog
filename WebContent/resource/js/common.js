42074fb65a6d7f740f52b9db916be3ec9b00f418
rekrcs <rekrcs@gmail.com>
2020-06-25 11:46:31 +0900

    모바일 탑, 사이드바 마무리

--- WebContent/resource/js/common.js
+++ WebContent/resource/js/common.js
@@ -1,37 +1,18 @@
-console.clear();
-
 function MobileSideBar__init() {
-  $('.mobile-top-bar .btn-toggle-mobile-side-bar').click(function() {
-    var $this = $(this);
-   if($this.hasClass('active')) {
-    $this.removeClass('active')
-    $('.mobile-side-bar').removeClass('active');
-   }
-   else {
-     $this.addClass('active')
-     $('.mobile-side-bar').addClass('active');
-   }
-  });
+    var $btnToggleMobileSideBar = $('.btn-toggle-mobile-side-bar');
+
+    $btnToggleMobileSideBar.click(function() {
+        if ( $(this).hasClass('active') ) {
+            $(this).removeClass('active');
+            $('.mobile-side-bar').removeClass('active');
+        }
+        else {
+            $(this).addClass('active');
+            $('.mobile-side-bar').addClass('active');
+        }
+    });
 }
 
 $(function() {
-  MobileSideBar__init();
+    MobileSideBar__init();
 });
-
-/*
-function a() {
-  $('div.abc').css('color', 'red');
-}
-
-$(function() {
-  // 프로그램 시작시에 실행되어야 하는 함수실행 코드 넣기
-  a();
-});
-*/
-
-/*
-// 특정 스크롤바 위치로 이동
-setTimeout(function() {
-  $(window).scrollTop(0);
-}, 500);
-*/
