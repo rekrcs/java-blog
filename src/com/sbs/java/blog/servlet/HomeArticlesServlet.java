facff486d86995f7304c970505edcb22e7cdb86a
rekrcs <rekrcs@gmail.com>
2020-06-26 16:50:23 +0900

    DB연결

--- src/com/sbs/java/blog/servlet/HomeArticlesServlet.java
+++ src/com/sbs/java/blog/servlet/HomeArticlesServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/articles")
+@WebServlet("/s/home/articles")
 public class HomeArticlesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
--- src/com/sbs/java/blog/servlet/HomeSnsServlet.java
+++ src/com/sbs/java/blog/servlet/HomeSnsServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/sns")
+@WebServlet("/s/home/sns")
 public class HomeSnsServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
